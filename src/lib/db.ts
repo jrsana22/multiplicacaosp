30c94c41675169dc086762dd688aee446df6fb69
Junior Sana <jrsana@yahoo.com.br>
2026-03-31 02:15:39 -0300

    Fix Prisma client initialization for production

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,13 +2,18 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
-  }
-  prisma = (global as any).prisma;
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ["error"],
+  });
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
+
+prisma = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export default prisma;
